51a8673580c8a326dc7599249da0e905e941b1ba
adog <adog@example.com>
2019-04-22 21:55:21 +0800

    complete

---
+++ src/main/java/com/imooc/springmvc/MySpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.imooc.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        //启动SpringBoot应用
+        SpringApplication.run(MySpringBootApplication.class);
+    }
+}
--- src/main/java/com/imooc/springmvc/controller/MyController.java
+++ src/main/java/com/imooc/springmvc/controller/MyController.java
@@ -1,5 +1,15 @@
 package com.imooc.springmvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class MyController {
+
+    @RequestMapping("/out")
+    @ResponseBody
+    public String out(){
+        return "success";
+    }
 }
